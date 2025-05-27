7fe905bff528a079e18057c44c9f99994ac0a5a2
mantasg6 <mantasgricius6@gmail.com>
2025-05-27 03:45:42 -0400

    spring init

---
+++ src/test/java/com/mantas/appointments/AppointmentSchedulingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mantas.appointments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentSchedulingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
