dc67c5a53ad751bb19ae3cfd6b4f8d7b4cfb5021
mantasg6 <mantasgricius6@gmail.com>
2025-06-03 10:07:57 -0400

    add generic authentication failure handler

--- src/main/java/com/mantas/appointments/exception/AuthExceptionHandler.java
+++ src/main/java/com/mantas/appointments/exception/AuthExceptionHandler.java
@@ -3,6 +3,7 @@ package com.mantas.appointments.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,6 +15,13 @@ public class AuthExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.UNAUTHORIZED)
                 .body("Invalid username or password");
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<String> handleAuthenticationFailure(AuthenticationException e) {
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body("Authentication failed: " + e.getMessage());
     }
 
 }
