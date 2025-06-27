606b96a44479a0c6590780d2384a93bc6f356a96
mantasg6 <mantasgricius6@gmail.com>
2025-06-27 11:38:19 -0400

    move role access tests

--- src/main/java/com/mantas/appointments/controller/ServicesController.java
+++ src/main/java/com/mantas/appointments/controller/ServicesController.java
@@ -1,15 +1,33 @@
 package com.mantas.appointments.controller;
 
+import com.mantas.appointments.entity.Service;
+import com.mantas.appointments.service.ServicesService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+/**
+ * Controller for managing services.
+ * Handles requests related to services.
+ */
 @RestController
 @RequestMapping("/api/v1/services")
+@RequiredArgsConstructor
 public class ServicesController {
 
+    private final ServicesService servicesService;
+
+    /**
+     * Retrieves all services.
+     *
+     * @return a list of all services
+     */
     @GetMapping
-    public String getAllServices() {
-        return "All Services";
+    public ResponseEntity<List<Service>> getAllServices() {
+        return ResponseEntity.ok(servicesService.getAllServices());
     }
 }
