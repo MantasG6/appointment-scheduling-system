19b03d1a5c7f285d7c34511f9968052d9826396e
Mantas <mantasgricius6@gmail.com>
2025-08-08 17:35:58 +0300

    add comments to util class

--- src/test/java/com/mantas/appointments/utils/TestUtils.java
+++ src/test/java/com/mantas/appointments/utils/TestUtils.java
@@ -7,18 +7,34 @@ import java.util.List;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
 
-
+/**
+ * Utility class for test-related methods.
+ */
 public final class TestUtils {
 
-    // Private constructor to prevent instantiation
+    /**
+     * Private constructor to prevent instantiation.
+     */
     private TestUtils() {
         throw new UnsupportedOperationException("Utility class");
     }
 
+    /**
+     * Creates a {@link JwtRequestPostProcessor} with the specified role.
+     *
+     * @param role the role to be added to the JWT
+     * @return a {@link JwtRequestPostProcessor} with the specified role
+     */
     public static JwtRequestPostProcessor jwtWithRole(String role) {
         return jwt().authorities(List.of(new SimpleGrantedAuthority("ROLE_" + role)));
     }
 
+    /**
+     * Generates an error message for when an entity is not found by its ID.
+     *
+     * @param id the ID of the entity that was not found
+     * @return a formatted error message
+     */
     public static String entityNotFoundMessage(Long id) {
         return "Entity not found with id: " + id;
     }
